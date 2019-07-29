909dbf5401629d1dd08b3db73481d254f8ec305a
Yasmeen <yasmine141297@hotmail.com>
2019-07-29 23:49:03 +0200

    Teefa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {HeroComponent} from './home/sign/hero.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path:'profile', component: ProfileComponent}
 ];
 
 @NgModule({
