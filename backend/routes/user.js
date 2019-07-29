33deb552e207df46ae4710d691b237c9139d0b8f
Saleh Elhadidy <saleh.mohamed2802@gmail.com>
2019-07-29 13:05:29 +0200

    added user login verification and sending uid

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -41,36 +41,38 @@ router.post('/login',(req,res,next) => {
   let fetchedUser;
   User.findOne({ email: req.body.email})
     .then(user => {
-      if(!user){
-        return res.status(401).json({
-          message: "Auth failed"
+      if(user == null){
+          return res.status(401).json({
+          message: "No Such Email"
         });
       }
+      else{
       fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then(result => {
-      if(!result) {
-        return res.status(401).json({
-          message: "Auth failed"
-        });
-      }
-      const token = jwt.sign(
-      {email: fetchedUser.email, userId: fetchedUser._id},
-      'secret_this_should_be_longer',
-      {expiresIn: "1h"}
-      );
-      res.status(200).json({
-        token: token,
-        expiresIn: 3600
+      passwordBoolean =  bcrypt.compare(req.body.password, user.password ,function(err,res2){
+        if(res2){
+          const token = jwt.sign(
+            {email: fetchedUser.email, userId: fetchedUser._id},
+            'secret_this_should_be_longer',
+            {expiresIn: "1h"}
+            );
+            return res.status(200).json({
+              token: token,
+              expiresIn: 3600,
+              id:fetchedUser._id
+            });
+        }
+        else if(!res2){
+            return res.status(401).json({
+            message: "Invalid authentication credentials!(Password)"
+          });
+        }
       });
+
+      
+      }
+      
+    
     })
-    .catch(err => {
-      console.log(err);
-      return res.status(401).json({
-      message: "Invalid authentication credentials!"
-    });
-  });
 });
 
 module.exports = router
