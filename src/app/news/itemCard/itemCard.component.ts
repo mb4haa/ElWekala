ddd910253b83ef47c21d3f30170eeef8a66f273b
mb4haa <houka97@gmail.com>
2019-07-31 00:14:56 +0200

    fixed loading news

--- src/app/news/itemCard/itemCard.component.ts
+++ src/app/news/itemCard/itemCard.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 // import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
-import {NewsServiceService} from '../news-service.service';
-import {Card} from '../card.modal';
+import { NewsServiceService } from '../news-service.service';
+import { Card } from '../card.modal';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
@@ -15,7 +15,7 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 export class itemCardComponent implements OnInit, OnDestroy {
 
   cards: Card[] = [];
-  cardSub = [1,2];
+  cardSub = [1, 2];
   BACKEND_URL = environment.url + 'product';
   sum = 100;
   throttle = 300;
@@ -23,52 +23,54 @@ export class itemCardComponent implements OnInit, OnDestroy {
   scrollUpDistance = 2;
   direction = '';
   page = 0;
-  likes: any[] =[]
-  
+  likes: any[] = []
 
-  constructor(private NewsService: NewsServiceService, private http: HttpClient) {}
+
+  constructor(private NewsService: NewsServiceService, private http: HttpClient) { }
   ngOnInit() {
     // this.NewsService.getPosts();
     // this.cardSub = this.NewsService.getPostUpdateListener().subscribe( (postData: { posts: Card[]}) => {
     //   this.card = postData.posts;
     //   console.log("this.card.length");
     // } );
-    this.likes = (localStorage.getItem('likes').split(","))
-    console.log(typeof(this.likes))
-    this.http.patch<{products: any}>(this.BACKEND_URL + '/getProducts', {pageNumber: this.page}).subscribe(res => {
+    if (localStorage.getItem('likes')) {
+      this.likes = (localStorage.getItem('likes').split(","));
+    }
+    console.log(typeof (this.likes))
+    this.http.patch<{ products: any }>(this.BACKEND_URL + '/getProducts', { pageNumber: this.page }).subscribe(res => {
       this.cards = res['products'];
-     // console.log(this.cards);
+      // console.log(this.cards);
     });
 
   }
-  rate(pid: any){
+  rate(pid: any) {
     let id = localStorage.getItem('uid')
-    this.http.patch<{}>(this.BACKEND_URL + '/likeProduct/'+pid, {token:localStorage.getItem('token'),uid:localStorage.getItem('uid')}).subscribe(res => {
-      if(res['message']=='Product updated'){
+    this.http.patch<{}>(this.BACKEND_URL + '/likeProduct/' + pid, { token: localStorage.getItem('token'), uid: localStorage.getItem('uid') }).subscribe(res => {
+      if (res['message'] == 'Product updated') {
         this.cards.forEach(element => {
-          if(element._id == pid){
-            element.likes+=1;
+          if (element._id == pid) {
+            element.likes += 1;
           }
         });
         this.likes.push(pid)
       }
     });
   }
-  unrate(pid: any){
+  unrate(pid: any) {
     let id = localStorage.getItem('uid')
-    this.http.patch<{}>(this.BACKEND_URL + '/unlikeProduct/'+pid, {token:localStorage.getItem('token'),uid:localStorage.getItem('uid')}).subscribe(res => {
-      if(res['message']=='Product updated'){
+    this.http.patch<{}>(this.BACKEND_URL + '/unlikeProduct/' + pid, { token: localStorage.getItem('token'), uid: localStorage.getItem('uid') }).subscribe(res => {
+      if (res['message'] == 'Product updated') {
         this.cards.forEach(element => {
-          if(element._id == pid){
-            element.likes-=1;
+          if (element._id == pid) {
+            element.likes -= 1;
           }
         });
         const x = this.likes.indexOf(pid)
-        this.likes.splice(x,1)
+        this.likes.splice(x, 1)
       }
     });
   }
-  test(pid:any){
+  test(pid: any) {
     this.likes = (localStorage.getItem('likes').split(","))
     console.log(this.likes)
   }
@@ -77,7 +79,7 @@ export class itemCardComponent implements OnInit, OnDestroy {
     this.page++;
     console.log(this.page);
     console.log('scrolled down!!', ev);
-    this.http.patch<{products: any}>(this.BACKEND_URL + '/getProducts', {pageNumber: this.page}).subscribe(res => {
+    this.http.patch<{ products: any }>(this.BACKEND_URL + '/getProducts', { pageNumber: this.page }).subscribe(res => {
       let temp: Card[] = [];
       temp = res['products']
       console.log(temp)
