2325a1f725bcdcd8c371e4839418b963a234d3bd
Saleh Elhadidy <saleh.mohamed2802@gmail.com>
2019-07-31 04:32:58 +0200

    added comment infrastructure

--- src/app/item-desc/item-desc.component.ts
+++ src/app/item-desc/item-desc.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-item-desc',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-desc.component.scss']
 })
 export class ItemDescComponent implements OnInit {
-
-  constructor() { }
+str:string
+  BACKEND_URL: string = environment.url;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
+comment(pid:any){
+ // console.log(this.str)
+ this.http.patch<{}>(this.BACKEND_URL + '/addComment  /'+pid, {id:localStorage.getItem('uid'),token:localStorage.getItem('token'),uid:localStorage.getItem('uid'),name:localStorage.getItem('name'),comment:this.str}).subscribe(res => {
+  if(res['message']=='Comment added'){
+    console.log("AS")
+  }
+  else{
 
+  }
+ });
+}
+doTextareaValueChange(event){
+  this.str = event.target.value
+}
 }
