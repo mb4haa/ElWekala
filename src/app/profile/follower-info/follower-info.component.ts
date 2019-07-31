25c93e3d1c2b749ea853ba1dfc908a7b2b93ed0d
MostafaAhmedAbbas <36128835+MostafaAhmedAbbas@users.noreply.github.com>
2019-07-31 04:22:05 +0200

    Please Ignore

---
+++ src/app/profile/follower-info/follower-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-follower-info',
+  templateUrl: './follower-info.component.html',
+  styleUrls: ['./follower-info.component.scss']
+})
+export class FollowerInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
